a8712d842ba8872bf67032d05cf3d78c4ee2ed25
97mams <97mams@gmail.com>
2024-04-18 18:52:53 +0300

    add layouts

---
+++ src/view/Footer.php
@@ -0,0 +1,5 @@
+</div>
+<script src="./assets/js/tailwindcss.js"></script>
+</body>
+
+</html>
---
+++ src/view/Header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentification</title>
+</head>
+
+<body class="bg-gray-950">
+    <div class="px-40">
+        <div class="w-full text-gray-400 flex justify-end gap-3 border-b border-gray-300 border-gray-800">
+            <a href="/?page=connect" class="hover:text-gray-600">Se connecter</a>
+            <a href="/?page=inscrire" class="hover:text-gray-600">S'inscrire</a>
+        </div>
