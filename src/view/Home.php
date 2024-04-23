1c8c4350bfe0b0cb40da05c613b194e1c89e582f
97mams <97mams@gmail.com>
2024-04-23 16:36:06 +0300

    add htmlentities for home page

--- src/view/Home.php
+++ src/view/Home.php
@@ -6,7 +6,7 @@ $user = App::getAut()->user();
 ?>
 <div class="w-full flex  justify-center p-5">
     <h1 class="text-4xl text-bold text-gray-100">
-        Salut <?php echo $user->username; ?> !!!
+        Salut <?php echo htmlentities($user->username); ?> !!!
         <br>
         Vous êtes connecté(e)
     </h1>
