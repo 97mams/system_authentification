23ff054ef87a80df7b57495c078a21d70e3d0c2d
97mams <97mams@gmail.com>
2024-04-29 07:12:49 +0300

    changed content in home

--- src/view/Home.php
+++ src/view/Home.php
@@ -6,8 +6,6 @@ $user = App::getAut()->user();
 ?>
 <div class="w-full flex  justify-center p-5">
     <h1 class="text-4xl text-bold text-gray-100">
-        Salut <?php echo htmlentities($user->username); ?> !!!
-        <br>
-        Vous êtes connecté(e)
+        Bienvenu <?php echo htmlentities($user->username); ?>
     </h1>
 </div>
