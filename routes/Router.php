a4accf325c5a5ff57f62ac0554dac7bb9573f901
97mams <97mams@gmail.com>
2024-05-24 09:58:46 +0300

    updated class route

--- routes/Router.php
+++ routes/Router.php
@@ -2,12 +2,42 @@
 
 namespace routes;
 
+use Exception;
+
 class Routes
 {
     private array $route;
 
+    /**
+     * @param string $path
+     * @param callable | [] $action
+     * @return void
+     */
     public function register(string $path, callable|array $action): void
     {
         $this->route[$path] = $action;
+    }
+
+    /**
+     * @param string $uri
+     * @return mixed
+     */
+    public function resoleve(string $uri): mixed
+    {
+        $path = explode("?", $uri)[0];
+        $action = $this->route[$path] ?? null;
+
+        if (is_callable($action)) {
+            return $action;
+        }
+
+        if (is_array($action)) {
+            [$className, $methodName] = $action;
+            if (class_exists($className) && method_exists($className, $methodName)) {
+                $class = new $className();
+                return call_user_func([$class, $methodName], []);
+            }
+        }
+        throw new Exception('route not found !!');
     }
 }
