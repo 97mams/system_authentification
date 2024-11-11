5f1cff17e3b93fb2535b5aff2435d8236f693d84
97mams <97mams@gmail.com>
2024-11-11 09:29:38 +0300

    has bug

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 
+use GuzzleHttp\Psr7\Header;
+
 require './../vendor/autoload.php';
 define('BASE_VIEW_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
 
 $route = new Route\Router($_SERVER['REQUEST_URI']);
 
-$route->get('/', function () {
-    echo 'mamisoa';
-});
+$route->get('/', function () {});
 
 $route->run();
