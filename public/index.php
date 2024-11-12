fe09b7fb82716a1eadcadf16ddabae05da0ea681
97mams <97mams@gmail.com>
2024-11-12 07:43:03 +0300

    add routing

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use GuzzleHttp\Psr7\ServerRequest;
+
 require './../vendor/autoload.php';
 
 define('BASE_VIEW_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views');
@@ -9,6 +11,7 @@ $route = new Route\Router($_SERVER['REQUEST_URI']);
 $route->get('/', ['controllers\HomeController', 'index']);
 $route->get('/singin', ['controllers\AuthController', 'singin']);
 $route->get('/login', ['controllers\AuthController', 'login']);
+$route->post('/verif', ['controllers\AuthController', 'doLogin']);
 
 try {
     echo $route->run();
