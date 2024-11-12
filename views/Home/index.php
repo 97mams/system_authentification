b19fc2b83408870008e9518952d908d3644a4ed9
97mams <97mams@gmail.com>
2024-11-12 07:01:30 +0300

    add icon

--- views/Home/index.php
+++ views/Home/index.php
@@ -1,7 +1,9 @@
 <?php
 require BASE_VIEW_PATH . DIRECTORY_SEPARATOR . 'Header.php'
 ?>
-<h1>Mamisoa</h1>
+<div class="w-full flex justify-center">
+    <h1 class="mt-80 text-5xl text-white uppercase"><span class="text-yellow-500">System</span> d'authentification</h1>
+</div>
 <?php
 require BASE_VIEW_PATH . DIRECTORY_SEPARATOR . '/Footer.php'
 ?>
